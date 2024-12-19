148571c623d5a7029cec353d79b03b3d88624f02
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2024-12-19 14:38:36 +0300

    first commit

---
+++ src/const.ts
@@ -0,0 +1,3 @@
+import * as anchor from "@coral-xyz/anchor";
+
+export const GAME_PROGRAM_ID = new anchor.web3.PublicKey("Gc1Cu95FyYE74ymk5yH5CdyLoVUJV7sxVJvCg7526Ece");
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,46 @@
-import { help } from "./helper";
+import { getLatestGameIndexFromAdminAccount, getMasterKeypair, welcome } from "./helper";
+import { sleep } from "./lib";
+import * as anchor from "@coral-xyz/anchor";
+import idl from "./data/idl.json";
+import { Connection, clusterApiUrl } from "@solana/web3.js";
+import { getOwnerWallet } from "./lib/wallets";
+import { GAME_PROGRAM_ID } from "./const";
+import { initializeAdmin, initializeGame } from "./lib/transactions";
 
-const x = help();
+console.log(welcome());
 
-console.log(x);
+// Owner keypair (admin)
+const ownerKeypair = getMasterKeypair();
+
+// Owner wallet (admin)
+const ownerWallet = getOwnerWallet();
+
+// Set up connection and provider
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+const provider = new anchor.AnchorProvider(connection, ownerWallet as any, {
+  preflightCommitment: "confirmed",
+});
+
+const program = new anchor.Program(idl as anchor.Idl, GAME_PROGRAM_ID, provider);
+
+async function transactions(): Promise<void> {
+  try {
+    await initializeAdmin(ownerKeypair, program);
+    const lastGameIndex = await getLatestGameIndexFromAdminAccount(ownerKeypair.publicKey, program);
+
+    await sleep(2000);
+    await initializeGame(ownerKeypair, program, lastGameIndex);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function runTransactions(interval: number): Promise<void> {
+  await transactions();
+  setInterval(async () => {
+    await transactions();
+  }, interval);
+}
+
+runTransactions(30000);
